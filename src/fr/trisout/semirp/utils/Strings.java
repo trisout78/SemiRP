d7e42ce94fad3db78e2223f726d6b8ffa5a7afbe
Trisout <81978700+trisout78@users.noreply.github.com>
2024-02-13 10:18:10 +0100

    Maven setup

--- src/fr/trisout/semirp/utils/Strings.java
+++
@@ -1,9 +0,0 @@
-package fr.trisout.semirp.utils;
-
-import org.bukkit.ChatColor;
-
-public class Strings {
-    public static String color(String string) {
-        return ChatColor.translateAlternateColorCodes('&', string);
-    }
-}
