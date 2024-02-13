3af306f5397e3f5daf8ea544501d197f5f9957d9
Trisout <81978700+trisout78@users.noreply.github.com>
2024-02-13 11:17:03 +0100

    Advancement worldborder

--- src/main/java/fr/trisout/semirp/Main.java
+++ src/main/java/fr/trisout/semirp/Main.java
@@ -1,14 +1,39 @@
 package fr.trisout.semirp;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.EventHandler;
 
 public class Main extends JavaPlugin {
+    private int worldBorder;
+
+    @Override
     public void onEnable() {
         System.out.println("§cBy Trisout");
         getCommand("Broadcast").setExecutor(new CommandBroadcast());
         getCommand("Annonce").setExecutor(new CommandAnnonce());
         getCommand("Mairie").setExecutor(new CommandMairie());
+
+        // Load world border value from config
+        this.worldBorder = this.getConfig().getInt("worldborder", 2000);
     }
+
+    @Override
     public void onDisable() {
         System.out.println("§cBy Trisout");
+    }
+
+    @EventHandler
+    public void OnPlayerAdvancementDone(PlayerAdvancementDoneEvent event) {
+        // Increment world border value
+        this.worldBorder++;
+
+        // Set new world border
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "worldborder set " + this.worldBorder);
+
+        // Save new world border value to config
+        this.getConfig().set("worldborder", this.worldBorder);
+        this.saveConfig();
     }
 }
