2303f6be69ce209356130453c29a8f5b918a47db
Trisout <81978700+trisout78@users.noreply.github.com>
2024-02-13 12:25:06 +0100

    Fix worldborder

--- src/main/java/fr/trisout/semirp/Main.java
+++ src/main/java/fr/trisout/semirp/Main.java
@@ -2,24 +2,30 @@ package fr.trisout.semirp;
 
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+import org.bukkit.advancement.Advancement;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.WorldBorder;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("Plugin By Trisout");
         getCommand("Broadcast").setExecutor(new CommandBroadcast());
         getCommand("Annonce").setExecutor(new CommandAnnonce());
         getCommand("Mairie").setExecutor(new CommandMairie());
     }
 
     @EventHandler
-    public void onAdvancementDone(PlayerAdvancementDoneEvent event) {
+    public void onAdvancementGranted(PlayerAdvancementDoneEvent event) {
+        getLogger().info("Advancement done");
         World world = Bukkit.getWorld("world");
         WorldBorder worldborder = world.getWorldBorder();
         double newsize = worldborder.getSize() + 1;
-        worldborder.setSize(newsize);
+        worldborder.setSize(newsize, 3);
     }
 }
