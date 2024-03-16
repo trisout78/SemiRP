8bfb78afc5c0a56d4d813f92b8b721e4425690b0
Trisout <81978700+trisout78@users.noreply.github.com>
2024-03-16 11:26:52 +0100

    Removed UseLess import

--- src/main/java/fr/trisout/semirp/Main.java
+++ src/main/java/fr/trisout/semirp/Main.java
@@ -5,7 +5,6 @@ import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
-import org.bukkit.advancement.Advancement;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.WorldBorder;
