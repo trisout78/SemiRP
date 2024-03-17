125fd261311400a0ecf239494564533e7ea2cd49
Trisout <81978700+trisout78@users.noreply.github.com>
2024-03-17 11:55:31 +0100

    Added onDisable method

--- src/main/java/fr/trisout/semirp/Main.java
+++ src/main/java/fr/trisout/semirp/Main.java
@@ -1,5 +1,6 @@
 package fr.trisout.semirp;
 
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -37,5 +38,20 @@ public class Main extends JavaPlugin implements Listener {
         double newsize = worldborder.getSize() + 1;
         worldborder.setSize(newsize, 3);
         getLogger().info("Advancement done by " + player + ". Setting world border to " + newsize + " blocks.");
+    }
+
+    @EventHandler
+    public void onDisable() {
+        getLogger().info("Plugin By Trisout");
+        getLogger().warning("Attention: Malgré le fait que ce plugin soit publique, merci de prendre en compte les information suivantes");
+        getLogger().warning("- Ce plugin est en développement, il est donc possible que des bugs soient présents");
+        getLogger().warning("- Vous devez impérativement me créditer si vous utilisez ce plugin");
+        getLogger().warning("- Vous ne pouvez pas vendre ce plugin");
+        getLogger().warning("- Vous devez, comment l'indique la licence, partager toute modification et sous la même licence");
+        getLogger().warning("- Vous ne pouvez pas retirer les crédits du plugin");
+        getLogger().warning("- Aucun support ne sera fourni pour ce plugin");
+        getLogger().warning("- Je ne suis pas responsable des dégâts que ce plugin pourrait causer");
+        getLogger().warning("- Aucune mise a jour de ce plugin est garantie");
+        getLogger().warning("- Vous ne pouvez pas supprimer cet avertissement");
     }
 }
